2e13c6c8ffc0eaf120bdb0b481fbb9e5156570af
wrgoldstein <williamrgoldstein@gmail.com>
2020-03-20 15:52:33 -0400

    Try adding google auth

--- src/routes/auth/index.svelte
+++ src/routes/auth/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import { stores, goto } from '@sapper/app';
+  import GoogleAuth from '@beyonk/svelte-social-auth/src/google-auth/GoogleAuth.svelte'
 
   const { session } = stores()
   const github_root = "https://github.com/login/oauth/authorize"
@@ -31,6 +32,21 @@
       goto('/')
     })
   }
+
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
 </script>
 
 {#if $session.user }
@@ -46,4 +62,4 @@
   {/if}
 </div>
 
-{loading_message}
+<GoogleAuth clientId="234393341639-ouolnt6vcbf1fcteebp9oa3racb9fphi" on:auth-success={e => console.dir(e.detail.user)} />
